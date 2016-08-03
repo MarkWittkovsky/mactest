b8e6d966d2ecd5cdcfe6a800d02510c66ec16288
Danny <dapahu@t-online.de>
2016-08-03 19:59:53 +0200

    Initial Commit

---
+++ Keksorakel/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Keksorakel
+//
+//  Created by Daniela on 03.08.16.
+//  Copyright © 2016 Daniela. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
